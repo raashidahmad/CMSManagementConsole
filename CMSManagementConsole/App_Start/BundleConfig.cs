8c87be2d9776c80355f6c0762d2c1d39120b01ba
Rashid Ahmad <raashid.ahmad@gmail.com>
2017-01-09 15:18:49 +0500

    Initial Commit

---
+++ CMSManagementConsole/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CMSManagementConsole
+    {
+    public class BundleConfig
+        {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+            {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.1.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/metisMenu.min.js",
+                      "~/Scripts/startmin.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/startmin.css",
+                      "~/Content/metisMenu.min.css",
+                      "~/Content/font-awesome/css/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/Content/logincss").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/bootstrap-theme.min.css",
+                      "~/Content/site.css"));
+            }
+
+            
+        }
+    }
---
+++ CMSManagementConsole/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMSManagementConsole.Controllers
+    {
+    public class HomeController : Controller
+        {
+        public ActionResult Index()
+            {
+            ViewBag.loginFailed = "";
+            return View("Login");
+            }
+
+        [HttpPost]
+        public ActionResult Login()
+            {
+            return RedirectToAction("Welcome");
+            }
+
+        public ActionResult Welcome()
+            {
+            return View();
+            }
+        
+        }
+    }
