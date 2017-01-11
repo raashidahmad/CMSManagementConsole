1b442e0cdd116c6ac1e226f814a189d93e6abda2
Rashid Ahmad <raashid.ahmad@gmail.com>
2017-01-11 15:07:32 +0500

    UI fixes and setup for connecting to Server APIs

--- CMSManagementConsole/Controllers/HomeController.cs
+++ CMSManagementConsole/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +9,14 @@ namespace CMSManagementConsole.Controllers
     {
     public class HomeController : Controller
         {
+        private string apiBaseUrl = "";
+        HttpClient client;
+
+        public HomeController()
+            {
+
+            }
+
         public ActionResult Index()
             {
             ViewBag.loginFailed = "";
