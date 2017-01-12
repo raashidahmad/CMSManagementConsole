7dcd77d89b16a6b9a206334fd3429a5d356cad5c
Rashid Ahmad <raashid.ahmad@gmail.com>
2017-01-12 22:54:52 +0500

    Added Login Logic

--- CMSManagementConsole/Controllers/HomeController.cs
+++ CMSManagementConsole/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using CMSManagementConsole.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace CMSManagementConsole.Controllers
@@ -14,7 +19,11 @@ namespace CMSManagementConsole.Controllers
 
         public HomeController()
             {
-
+            apiBaseUrl = WebConfigurationManager.AppSettings["ApiBaseUrl"];
+            client = new HttpClient();
+            client.BaseAddress = new Uri(apiBaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             }
 
         public ActionResult Index()
@@ -23,10 +32,52 @@ namespace CMSManagementConsole.Controllers
             return View("Login");
             }
 
-        [HttpPost]
         public ActionResult Login()
             {
-            return RedirectToAction("Welcome");
+            return View();
+            }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+            {
+            ViewBag.loginFailed = "";
+            if (!ModelState.IsValid)
+                {
+                return View(model);
+                }
+
+            try
+                {
+                var formContent = new FormUrlEncodedContent(new[]
+                 {
+                 new KeyValuePair<string, string>("grant_type", "password"), 
+                 new KeyValuePair<string, string>("username", model.Username), 
+                 new KeyValuePair<string, string>("password", model.Password), 
+                 });
+
+                HttpResponseMessage responseMessage = await client.PostAsync("/Token", formContent);
+
+                //get access token from response body
+                var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                var jObject = JObject.Parse(responseJson);
+                string accessToken = "";
+                if (jObject.GetValue("access_token") != null)
+                    {
+                    accessToken = jObject.GetValue("access_token").ToString();
+                    }
+                else
+                    {
+                    ViewBag.loginFailed = "Username/Password you entered is incorrect.";
+                    return View("Login");
+                    }
+                Session["accessToken"] = accessToken;
+                return RedirectToAction("Welcome");
+                }
+            catch (Exception ex)
+                {
+                ViewBag.loginFailed = ex.Message;
+                }
+            return View("Login");
             }
 
         public ActionResult Welcome()
